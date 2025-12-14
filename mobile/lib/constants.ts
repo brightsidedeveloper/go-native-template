81b27e59916eae60349919c50eda9b341e8254be
tim vanlerberg <tim@brightsidedeveloper.com>
2025-12-14 17:11:38 -0600

    constant

--- mobile/lib/constants.ts
+++ mobile/lib/constants.ts
@@ -1,3 +1,1 @@
-const PROD = 'https://gql.brightsideserve.com/query'
-
-export const HTTP_ENDPOINT = PROD
+export const HTTP_ENDPOINT = 'http://[RUN make port in server folder to get the IP]:8080/query'
